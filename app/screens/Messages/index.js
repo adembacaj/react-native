88e8475a1bcb26e8e73b4e92015a2b8a2311c3cd
adembacaj <adibacaj@live.com>
2020-03-05 17:44:24 +0100

    a bug fixed

--- app/screens/Messages/index.js
+++ app/screens/Messages/index.js
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { connect } from 'react-redux';
 import styles from './style.js';
 import images from '../../assets/images';
